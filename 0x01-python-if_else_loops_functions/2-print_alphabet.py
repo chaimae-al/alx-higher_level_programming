493e4baaf6a31f48e7b390a4d614a6ec9760a135
chaimae-al <chaimaehachimi01@gmail.com>
2023-06-06 21:19:06 +0100

    ok

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for alphabet in range(97,123):
-    print("{}".format(chr(alphabet)),end="")
+for alphabet in range(97, 123):
+    print("{}".format(chr(alphabet)), end="")
     
