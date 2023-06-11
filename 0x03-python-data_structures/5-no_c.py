c925e4509bd17ad8f49bc7b2ec03dc3f4d277c2b
chaimae-al <chaimaehachimi01@gmail.com>
2023-06-11 19:39:41 +0100

    ok

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@
 def no_c(my_string):
     str_list = list(my_string)
     for i in range(len(str_list)):
-        if str_list[i] == 'c':
+        if str_list[i] == 'c' or str_list[i] == 'C':
             str_list[i] = ''
     modified_str = ''.join(str_list)
     return modified_str
