44808a3b240f3e56968ade47d488901a7b175c8a
chaimae-al <chaimaehachimi01@gmail.com>
2023-06-11 19:55:12 +0100

    ok

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx in range(len(row)):
-            print("{:d}".format(row[idx]),
-          end=" " if idx + 1 < len(row) else '\n')
+            print("{:d}".format(row[idx]), end=" " if idx + 1 < len(row)
+                  else '\n')
