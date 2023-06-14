bb0cc51dce607786f578b4aebb5f7752001bed66
chaimae-al <chaimaehachimi01@gmail.com>
2023-06-15 00:52:18 +0100

    ok

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary):
 
     if a_dictionary:
-        my_list = list(a_dictionary.key())
+        my_list = list(a_dictionary.keys())
         score = 0
         leader = ""
         for i in my_list:
