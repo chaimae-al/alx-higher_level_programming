5b640e4df438503e3ace5e74280eede882912a0d
chaimae-al <chaimaehachimi01@gmail.com>
2023-10-01 11:30:58 +0100

    ok

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Send a GET request to a given URL with a header variabl
+e
+curl -sH "X-School-User-Id: 98" "${1}"
